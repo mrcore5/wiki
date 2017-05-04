0547653fd1fbce972452b1087074a7b1f835c7e5
Matthew Reschke <mail@mreschke.com>
2017-05-04 15:56:48 -0600

    Table css fix

--- Views/file/detail.blade.php
+++ Views/file/detail.blade.php
@@ -8,96 +8,104 @@
 
 <div class="fm-content">
 
-    <div class="panel panel-default">
+    {{-- Do NOT show panel, or row or col-md-12 if embedded (in wiki page) or else it flows past the TOC --}}
+    @if(!$params['embed'])
+        <div class="panel panel-default">
         <div class="panel-body">
-            @if(!$params['nomenu'])
-                @yield('menubar')
-            @endif
+    @endif
 
-            <div class="row">
-                <div class="col-md-12 ">
-                    @if(!$params['nonav'])
-                        @yield('navbar')
-                    @endif
-                </div>
+    @if(!$params['nomenu'])
+        @yield('menubar')
+    @endif
+
+    @if(!$params['nonav'])
+        <div class="row">
+            <div class="col-md-12 ">
+                @yield('navbar')
             </div>
-
-            <div class="row">
-                <div class="col-md-12">
-
-                    <div class="fm-files">
-                        <table class="fm-table table table-condensed table-striped table-hover {{ $params['embed'] ?: 'dataTable' }}">
-                            <thead>
-                            <tr>
-                                <th width="5"></th>
-                                <th>File</th>
-                                <th width="5">KB</th>
-                                <th width="5">Type</th>
-                                <th width="5">Date</th>
-                                <th width="5"></th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            @foreach ($dir->getChildren() as $child)
-                                <tr>
-                                @if (isset($child->url))
-                                    {{-- Directory --}}
-                                    <td align="center">
-                                        <i class="fa fa-folder"></i>
-                                    </td>
-                                    <td>
-                                        <a href="{{ $url->getLink($child->getName()) }}">
-                                            {{ $child->getName() }}
-                                        </a>
-                                    </td>
-                                    <td></td>
-                                    <td>Folder</td>
-                                    <td>
-                                        {{ \Carbon\Carbon::createFromTimeStamp($child->getlastModified()) }}
-                                    </td>
-                                    <td>
-                                        @if ($url->isWritable() && !$params['embed'])
-                                            <button class="delete-btn btn btn-xs btn-danger" data-filename="{{ $child->getName() }}"><i class="fa fa-remove"></i></button>
-                                        @endif
-                                    </td>
-                                @else
-                                    {{-- File --}}
-                                    <td align="center">
-                                        <i class="fa fa-file-o"></i>
-                                    </td>
-                                    <td>
-                                        <a href="{{ $url->getLink($child->getName()) }}" target="_blank">
-                                            {{ $child->getName() }}
-                                        </a>
-                                    </td>
-                                    <td>
-                                        {{ round($child->getSize() / 1024, 2) }}
-                                    </td>
-                                    <td>
-                                        {{ $child->getExtension() }}
-                                    </td>
-                                    <td>
-                                        {{ \Carbon\Carbon::createFromTimeStamp($child->getlastModified()) }}
-                                    </td>
-                                    <td>
-                                        @if ($url->isWritable() && !$params['embed'])
-                                            <button class="delete-btn btn btn-xs btn-danger" data-filename="{{ $child->getName() }}"><i class="fa fa-remove"></i></button>
-                                        @endif
-                                    </td>
-                                @endif
-                            </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
-                        {{-- var_dump($dir->getChildren()) --}}
-                        {{-- var_dump($url) --}}
-                    </div>
-
-                </div>
-            </div>
-
         </div>
+    @endif
+
+    @if(!$params['embed'])
+        <div class="row">
+        <div class="col-md-12">
+    @endif
+
+    <div class="fm-files">
+        <table class="fm-table table table-condensed table-striped table-hover {{ $params['embed'] ?: 'dataTable' }}">
+            <thead>
+            <tr>
+                <th width="5"></th>
+                <th>File</th>
+                <th width="5">KB</th>
+                <th width="5">Type</th>
+                <th width="5">Date</th>
+                <th width="5"></th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach ($dir->getChildren() as $child)
+                <tr>
+                @if (isset($child->url))
+                    {{-- Directory --}}
+                    <td align="center">
+                        <i class="fa fa-folder"></i>
+                    </td>
+                    <td>
+                        <a href="{{ $url->getLink($child->getName()) }}">
+                            {{ $child->getName() }}
+                        </a>
+                    </td>
+                    <td></td>
+                    <td>Folder</td>
+                    <td>
+                        {{ \Carbon\Carbon::createFromTimeStamp($child->getlastModified()) }}
+                    </td>
+                    <td>
+                        @if ($url->isWritable() && !$params['embed'])
+                            <button class="delete-btn btn btn-xs btn-danger" data-filename="{{ $child->getName() }}"><i class="fa fa-remove"></i></button>
+                        @endif
+                    </td>
+                @else
+                    {{-- File --}}
+                    <td align="center">
+                        <i class="fa fa-file-o"></i>
+                    </td>
+                    <td>
+                        <a href="{{ $url->getLink($child->getName()) }}" target="_blank">
+                            {{ $child->getName() }}
+                        </a>
+                    </td>
+                    <td>
+                        {{ round($child->getSize() / 1024, 2) }}
+                    </td>
+                    <td>
+                        {{ $child->getExtension() }}
+                    </td>
+                    <td>
+                        {{ \Carbon\Carbon::createFromTimeStamp($child->getlastModified()) }}
+                    </td>
+                    <td>
+                        @if ($url->isWritable() && !$params['embed'])
+                            <button class="delete-btn btn btn-xs btn-danger" data-filename="{{ $child->getName() }}"><i class="fa fa-remove"></i></button>
+                        @endif
+                    </td>
+                @endif
+            </tr>
+            @endforeach
+            </tbody>
+        </table>
+        {{-- var_dump($dir->getChildren()) --}}
+        {{-- var_dump($url) --}}
     </div>
+
+    @if(!$params['embed'])
+        </div>
+        </div>
+        </div>
+        </div>
+    @endif
+
 </div>
 
 <div class="modal" id='upload-modal'>
