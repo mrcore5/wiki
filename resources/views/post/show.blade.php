5bd9fd5a749d4c582ee3a69f4911807d8285a30f
Matthew Reschke <mail@mreschke.com>
2018-10-22 22:43:16 -0600

    Added panel to post body

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -13,13 +13,28 @@
         @else
             <form method="post">
 
-                @if ($post->type->constant == 'doc')
-                    @yield('post-header')
-                @endif
+                @if ($post->type->constant == 'app')
 
-                <div class="post-content {{ $post->format->constant }}">
-                    {!! $post->content !!}
-                </div>
+                    <div class="post-content {{ $post->format->constant }}">
+                        {!! $post->content !!}
+                    </div>
+
+                @else
+
+                    <div class="panel">
+                        <div class="panel-body">
+
+                            @if ($post->type->constant == 'doc')
+                                @yield('post-header')
+                            @endif
+
+                            <div class="post-content {{ $post->format->constant }}">
+                                {!! $post->content !!}
+                            </div>
+                        </div>
+                    </div>
+
+                @endif
 
             </form>
         @endif
